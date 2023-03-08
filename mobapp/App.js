845ee8d7220a3fe467b4f0d2f73a6e132344dcf0
nyomangedar <nyoman.gedar@gmail.com>
2023-03-08 21:53:18 +0000

    succesfully fetch api, but promise still bugging

--- mobapp/App.js
+++ mobapp/App.js
@@ -11,11 +11,12 @@ import { Camera, CameraType } from "expo-camera";
 import { useState, useEffect, useRef } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import * as Speech from "expo-speech";
+import * as FileSystem from "expo-file-system"
 // import { setIntervalAsync, clearIntervalAsync } from "set-interval-async";
 
 export default function App() {
     const [hasCameraPermission, setHasCameraPermission] = useState(null);
-    const [image, setImage] = useState(null);
+    const [waiting, setWaiting] = useState(false);
     const [type, setType] = useState(Camera.Constants.Type.back);
     const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
     const [data, setData] = useState("");
@@ -24,32 +25,67 @@ export default function App() {
     const { height, width } = Dimensions.get("window");
     let loop;
 
+
+    const request = async (file) => {
+        const res = await FileSystem.uploadAsync(
+            "http://192.168.0.21:8000/uploadfile",
+            file,
+            {
+              uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+              fieldName: 'file',
+              mimeType: 'image/png',
+            });
+        console.log("didalem request",{waiting})
+        return res.body
+
+    }
     const setupCamera = async () => {
         const cameraStatus = await Camera.requestCameraPermissionsAsync();
         setHasCameraPermission(cameraStatus.status === "granted");
     };
 
+    const speak = (data) => {
+        Speech.speak(data, {
+            pitch: 1,
+        });
+    };
+
     const beginCapture = async () => {
+        if(capture){
+            speak("Stop Capturing Image")
+        }
+        else{
+            speak("Begin Capturing Image")
+        }
+        setCapture(!capture)
+        // const image = await takePicture()
+        // const test =  await request(image.uri, test)
+        // console.log(test)
+       
+    }
+
+    const captureFunction = async () => {
         if (capture) {
-            console.log("Start Request");
-            console.log(image);
-            await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-                method: "POST",
-            })
-                .then((response) => response.json())
-                .then((json) => setData(json));
+            if (waiting === true){return}
+            const image = await takePicture()
+            console.log({waiting})
+            const data = await request(image.uri)
+            if(data){
+                console.log(data)
+                speak(data)
+                setData(data)
+                setWaiting(false)
+            }
+            
         }
     };
 
-    const loopFunction = () => {
-        loop = setInterval(async () => {
+    const loopFunction = async () => {
+       loop = setInterval(async () => {
             // console.log(capture);
-
-            await takePicture();
-
-            await beginCapture();
+            await captureFunction();
             console.log("finish one request");
-        }, 1000);
+        }, 5000);
         if (capture === false) {
             clearInterval(loop);
         }
@@ -58,6 +94,7 @@ export default function App() {
         loopFunction();
         return () => {
             clearInterval(loop);
+            setData("")
         };
     }, [capture]);
 
@@ -65,24 +102,22 @@ export default function App() {
         setupCamera();
     }, []);
 
+
     const takePicture = async () => {
         if (cameraRef) {
             try {
                 console.log("start test");
+                setWaiting(true)
+                console.log("waiting di picture", waiting)
                 const image = await cameraRef.current.takePictureAsync();
-                setImage(image);
-                console.log("test" + image);
+                return image
             } catch (error) {
                 console.log(error);
             }
         }
     };
 
-    const speak = () => {
-        Speech.speak(data, {
-            pitch: 1,
-        });
-    };
+    
 
     const message = () => {
         if (data.length !== 0) {
@@ -112,8 +147,8 @@ export default function App() {
                 <Text>{capture && message()}</Text>
                 <Button
                     title="begin capture"
-                    // onPress={() => setCapture(!capture)}
-                    onPress={speak}
+                    onPress={() => beginCapture()}
+                    // onPress={speak}
                 >
                     Begin
                 </Button>
